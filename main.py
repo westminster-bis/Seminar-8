c22397e4de3da342f2a8cb46505b457bd4205009
00016495 <00016495wiut@gmail.com>
2023-11-01 18:03:04 +0500

    InitialCommit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Create two tuples with student information
+student1 = (1, 20, "Tashkent")
+student2 = (2, 22, "Tashkent")
+
+# Compare the information of the two students
+if student1 == student2:
+    print("Both students have the same information.")
+else:
+    print("The students have different information.")
+
+# Access information for each student
+print("Student 1 - ID:", student1[0])
+print("Student 1 - Age:", student1[1])
+print("Student 1 - Hometown:", student1[2])
+
+print("Student 2 - ID:", student2[0])
+print("Student 2 - Age:", student2[1])
+print("Student 2 - Hometown:", student2[2])
+
